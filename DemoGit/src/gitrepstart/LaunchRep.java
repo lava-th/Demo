7f18f166c42934d3fa49c5216fd693cc2abf5004
lava-th <78860302+lava-th@users.noreply.github.com>
2021-06-06 16:56:11 +0530

    first commit

---
+++ DemoGit/src/gitrepstart/LaunchRep.java
@@ -0,0 +1,10 @@
+package gitrepstart;
+
+public class LaunchRep {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello welcome to version control system");
+	}
+
+}
